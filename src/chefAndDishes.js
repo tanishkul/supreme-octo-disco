bb7ebcee2090837376541a5c4c9e804c11d4c6ba
tanishkul <tanishkul@gmail.com>
2021-06-14 19:52:03 +0530

    Added three logical questions

---
+++ src/chefAndDishes.js
@@ -0,0 +1,15 @@
+ingred: 4
+dish 2nd: 
+total ingred?
+
+input: 4
+dish: 2
+
+
+Dish 1: [1, 2, 4] = 7
+Dish 2: [1, 1, 2, 1, 2, 4]= 11
+Dish 3: [1, 1, 1, 2, 1, 1, 2, 1, 2, 4] = ??
+
+ingred: 10
+dish 3: 
+total ingred?
---
+++ src/trees.js
@@ -0,0 +1,15 @@
+tree V
+(V+1)%M
+[0, (V+1)%M-1]
+
+V=2
+tree=1
+
+
+trees: 1
+M=4
+=> 3
+
+trees cut: 0
+M=5
+=>
