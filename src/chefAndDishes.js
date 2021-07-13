97d8662caf98191b9a4de323633913de549a27f8
tanishkul <tanishkul@gmail.com>
2021-07-13 20:30:28 +0530

    Added Chef and Dishes problem's solution

--- src/chefAndDishes.js
+++ src/chefAndDishes.js
@@ -1,4 +1,4 @@
-ingred: 4
+/* ingred: 4
 dish 2nd: 
 total ingred?
 
@@ -12,4 +12,23 @@ Dish 3: [1, 1, 1, 2, 1, 1, 2, 1, 2, 4] = ??
 
 ingred: 10
 dish 3: 
-total ingred?
+total ingred?*/
+
+const getFactor = (num) => {
+    return [...Array(num + 1).keys()].filter(i => num % i === 0);
+}
+
+function dishes(dishNo, ingredientNo) {
+    let a = getFactor(ingredientNo);
+    let result = [...a];
+    [...Array(dishNo - 1).keys()].forEach(() => {
+        let temp = [];
+        result.forEach(e2 => {
+            temp = [...temp, ...getFactor(e2)];
+        })
+        result = [...temp];
+    });
+    console.log('Dish no:', dishNo, '\nResult:', result, '\nSum', result.reduce((ele, acc) => acc = acc + ele));
+}
+dishes(2, 4);
+dishes(3, 10);
