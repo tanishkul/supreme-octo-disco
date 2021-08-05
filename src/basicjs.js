be7e19279d983160bf7985658dacf70cdd9c84d6
tanishkul <tanishkul@gmail.com>
2021-08-05 20:09:43 +0530

    Added more basics js

--- src/basicjs.js
+++ src/basicjs.js
@@ -64,3 +64,107 @@ async function greetV2() {
   console.log(`Ans 7 & 8: ${h} ${w}`);
 }
 greetV2();
+
+let car = {
+  registrationNumber: "GA12345",
+  brand: "Toyota",
+  displayDetails: function () {
+      console.log(this.registrationNumber + " " + this.brand);
+  }
+}
+let myCarDetails = car.displayDetails;
+console.log('**********', myCarDetails(), typeof myCarDetails);
+// // output ?
+
+const arr = [1, 2, 3, 4 , 5, 1, 2, 3, 3];
+
+let count = {};
+for (let i=0;i<arr.length;i++) {
+  if(!count[arr[i]]) {
+      count[arr[i]] = 1;
+  } else {
+      count[arr[i]] += 1;
+  }
+}
+//count = {1: 2, 2: 2, 3: 3, 4:1, 5:1};
+const updatedARray = []
+for(let key in count) {
+  if(count[key] === 2) {
+      updatedARray.push(key);
+  }
+}
+console.log('1111111111', updatedARray)
+console.log('Start') 
+setTimeout(()=>{
+console.log('Set Timeout 1')
+},0)
+
+setTimeout(()=>{
+console.log('Set Timeout 2')
+},0)
+
+Promise.resolve().then(r => console.log('Promise 1')).then( s=> console.log('Promise 2'));
+console.log('End')
+
+
+let num = 0
+async function increment(){
+num+= await 2
+console.log(num)
+}
+increment()
+num+=1
+console.log(num)
+// Print the output
+
+
+// user: {
+//     Id
+//     name, 
+// }
+
+// booking: {
+//     booking Id
+//     userId
+//     startStation
+//     EndStattion
+//     amount
+//     distance
+//     class
+//     seatId,
+//     timestamp
+// }
+
+// seat: {
+//     isBOOked
+//     Id
+//     Number
+//     coach
+// }
+
+// const seat = seatModel.find({number: userRequestedSeatNumber });
+// if(seat) {
+//     throw 400 badrequuest
+// } else {
+//     const seatCreated = await seatModel.update({number: userRequestedSeatNumber  }, {isBooked: true, ...});
+//     await bookingModel.create({ seat : seatCreated, ..})
+// }
+// console.log(this);
+
+let array = [4, 2, [3, 4, 5, 7], 9, [[10], 12, 3], 4, 7];
+
+function flatten(items) {
+  const flat = [];
+
+  items.forEach(item => {
+    if (Array.isArray(item)) {
+      flat.push(...flatten(item));
+    } else {
+      flat.push(item);
+    }
+  });
+
+  return flat;
+}
+
+console.log(flatten(array));
